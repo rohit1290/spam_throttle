f8d5843e43ee247ec115d478a9955df610da3226
Rohit Gupta <rohit_1290@yahoo.com>
2024-08-18 21:23:44 +0530

    release 6.0

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -1,12 +1,11 @@
 <?php
 require_once __DIR__ . '/lib/functions.php';
-require_once __DIR__ . '/lib/hooks.php';
 require_once __DIR__ . '/lib/events.php';
 
 return [
 	'plugin' => [
 		'name' => 'Spam Throttle',
-		'version' => '5.0',
+		'version' => '6.0',
 		'dependencies' => [],
 	],
 	'bootstrap' => SpamThrottle::class,
